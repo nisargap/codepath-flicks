50f117a770542c03f3b043c3818599a3f47f4804
Nisarga Patel <np8@umbc.edu>
2016-02-02 20:38:12 -0500

    first commit

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Nisarga Patel on 2/1/16.
+//  Copyright © 2016 Nisarga Patel. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var overviewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
